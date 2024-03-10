e6e04a1e1b1a409d659c68d451f791489976a138
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-03-10 18:13:12 +0000

    Add first files

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+canonical() {
+  echo $(eval dirname "$1")/$(basename "$1")
+}
+
+PROJECT_ROOT=https://github.com/marcelobojikian/gene/raw/main
+
+COMMAND_NAME=gene
+COMMAND_PATH_CONF=$(canonical "~/.$COMMAND_NAME")
+
+echo Instalando Home Lab library
+
+echo Config Path: "$COMMAND_PATH_CONF"
+mkdir -p "$COMMAND_PATH_CONF"
+
+echo Set default configuration on path $COMMAND_PATH_CONF/conf.txt
+cat <<EOF > $COMMAND_PATH_CONF/conf.txt
+URI=$PROJECT_ROOT/src
+FUNCTIONS=$COMMAND_PATH_CONF/cache/global/functions.sh
+LOG_LEVEL=ERROR
+EOF
+
+echo Download command $COMMAND_NAME from $PROJECT_ROOT
+
+TO=/usr/local/sbin/$COMMAND_NAME
+sudo curl -sSLo "$TO" --progress-bar "$PROJECT_ROOT/src/command.sh" && sudo chmod +x "$TO"
+
+echo Enable cache on path $COMMAND_PATH_CONF/cache
+$COMMAND_NAME cache enable --url="$PROJECT_ROOT/src" --path="$COMMAND_PATH_CONF/cache"
+
+echo Instalacao completa
---
+++ src/global/functions.sh
@@ -0,0 +1,81 @@
+
+command_key(){
+
+  declare -A targets
+  
+  targets[cache]=global
+  targets[first-installer]=proxmox
+
+  echo "${targets[$1]}/$1.sh"
+}
+
+log() {
+  
+  local severity=$(echo "${1}" | awk '{print toupper($0)}')
+  shift
+
+  local -A levels=( ['DEBUG']="7" ['INFO']="6" ['WARN']="4" ['ERROR']="3" ['FATAL']="2")
+  [[ ! ${!levels[@]} =~ $severity ]] && echo "Invalid log" && exit 1
+
+  local LEVEL=$(echo "${LOG_LEVEL:-"ERROR"}" | awk '{print toupper($0)}')
+  local LOG="${levels[${LEVEL}]}"
+
+  local lvl_msg="${levels[${severity}]:-3}"
+  local msg="[${severity}] ${@}";
+
+  [ $LOG -ge $lvl_msg ] && echo -e $msg
+
+}
+
+download() {
+
+    local URL=$URI/$1
+    local DEST_FILE=$2
+    
+    local HTTP_CODE=$(curl -sSLo "$DEST_FILE" -w "%{http_code}" "$URL")
+
+    if [ ${HTTP_CODE} -eq 404 ] ; then
+      rm -f "$DEST_FILE"
+      echo "Url not found: $URL" && exit 1
+    elif [ ${HTTP_CODE} -ne 200 ] ; then
+      rm -f "$DEST_FILE"
+      echo "Downloaded fail: $URL" && exit 2
+    fi
+
+}
+
+cmd_cache() {
+  
+  DEFAULT_CACHE_CONF=~/.gene/cache/conf.txt
+  path_caching=
+  key_caching=global/cache.sh
+
+  [ ! -f "$DEFAULT_CACHE_CONF" ] && exit 1
+
+  CACHE_PATH=$(get_config "$DEFAULT_CACHE_CONF" "PATH")
+  CANONICAL_CACHING=$(eval dirname $CACHE_PATH)/$(basename $CACHE_PATH)
+  path_caching="$CANONICAL_CACHING/global/cache.sh"
+
+  if [ ! -f $path_caching ] ; then
+
+    local KEY_CACHE="$(command_key cache)"
+    
+    mkdir -p $(dirname "$path_caching")
+    download "$key_caching" "$path_caching"
+    chmod +x "$path_caching"
+
+  fi
+  
+  local KEY=$1
+
+  cmd_cached=$($path_caching get $KEY 2> /dev/null)
+  if [ $? -ne 0 ] ; then
+    local FILE=$(mktemp -u)
+    download "$KEY" "$FILE"
+    cmd_cached=$($path_caching put "$KEY" "$FILE")
+    chmod +x "$cmd_cached"
+  fi
+
+  echo $cmd_cached
+
+}
