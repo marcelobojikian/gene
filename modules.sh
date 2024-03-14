0463cc8e554e39b3a497be9b226ff3f93ba29ab2
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-03-14 21:29:06 +0000

    Updated to be more readable

--- modules.sh
+++ modules.sh
@@ -1,5 +1,40 @@
 #!/usr/bin/env bash
+##########################################################################
+# Shellscript:	synopsis - installs module dependencies
+# Author     :	Heiner Steven <heiner.steven@odn.de>
+# Date       :	2024-03-14
+# Category   :	System Test
+##########################################################################
+# Reference
+# bats-core  : https://bats-core.readthedocs.io/en/stable/tutorial.html
+##########################################################################
+# Description
+#   Installs the modules needed for the programme's code and tests
+#
+##########################################################################
 
-git submodule add -f https://github.com/bats-core/bats-core.git third/bats
-git submodule add -f https://github.com/bats-core/bats-support.git third/test_helper/bats-support
-git submodule add -f https://github.com/bats-core/bats-assert.git third/test_helper/bats-assert
+set -e
+
+################################################# variables
+
+###########################################################
+
+################################################# functions
+gitMudule() {
+    git submodule add -f "https://github.com/$1.git third/$2"
+}
+
+###########################################################
+
+############################################## dependencies
+for check in 'git' ; do
+    if ! which "$check" &> /dev/null
+        then echo "Must have $check installed" && exit 1
+    fi
+done
+###########################################################
+
+################################################# Principal
+gitMudule bats-core/bats-core bats
+gitMudule bats-core/bats-support test_helper/bats-support
+gitMudule bats-core/bats-assert test_helper/bats-assert
