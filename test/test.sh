cf689eedb1d4db824be8fe702396e03483266ce3
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-03-14 21:32:20 +0000

    Add Header and fixed method launch

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
-# https://bats-core.readthedocs.io/en/stable/tutorial.html
+##########################################################################
+# Shellscript:	synopsis - test all file using bats
+# Author     :	Heiner Steven <heiner.steven@odn.de>
+# Date       :	2024-03-14
+# Category   :	System Test
+##########################################################################
+# Reference
+# bats-core  : https://bats-core.readthedocs.io/en/stable/tutorial.html
+##########################################################################
+# Description
+#   Exports the project location variable.
+#   Launches all tests by file, folder or tags.
+#
+##########################################################################
 
 export PROJECT_ROOT="$( cd "$(pwd)/.." >/dev/null 2>&1 && pwd )"
 launcher="$PROJECT_ROOT/third/bats/bin/bats"
 
+launch(){ [ $# -eq 0 ] && $launcher -prT "$PROJECT_ROOT/test" || $launcher -pT "$PROJECT_ROOT/test/$1" ; }
 launch_tag(){ $launcher -pTr "$PROJECT_ROOT/test" --filter-tags "$1"; }
-launch(){ [ $# -eq 0 ] && $launcher -prT "$PROJECT_ROOT/test/global/cache" || $launcher -pT "$PROJECT_ROOT/test/$1" ; }
 
 server_pid=
 server_on(){
@@ -18,5 +31,5 @@ server_off(){ kill $server_pid; }
 launch_tag !server
 
 server_on
-launch_tag server
+    launch_tag server
 server_off
