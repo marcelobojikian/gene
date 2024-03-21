db512a2c589564dc761f9e342b1467991d24d2b0
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-03-21 03:25:16 +0000

    Portable gene.sh, tested with shellcheck

--- src/gene.sh
+++ src/gene.sh
@@ -17,14 +17,11 @@
 readonly VERSION=1.0
 readonly IMPORT_PATH=$HOME/.gene/import
 readonly LANG=$(locale | grep LANGUAGE | cut -d= -f2 | cut -d_ -f1)
-
-readonly GITHUB_SRC="https://github.com/%s/raw/main/src/%s"
-readonly GITHUB_USAGE="https://github.com/%s/raw/main/usage/${LANG}/%s"
 ###########################################################
 
 ############################################## dependencies
-for check in 'curl' ; do
-    if ! which "$check" &> /dev/null
+for check in "curl" ; do
+    if ! which "$check" > /dev/null 2>&1
         then echo "Must have $check installed" && exit 1
     fi
 done
@@ -49,28 +46,28 @@ For more details, see man gene.
 EOF
   exit 0; 
 }
-die() { printf "${@}\nTry 'gene -h' for more information.\n" && exit 1 ; }
+die() { printf "%s\nTry 'gene -h' for more information.\n" "${@}" && exit 1 ; }
 
 download() { # ${1} URL; ${2} FILE
-  local tempFile=$(mktemp -u)
-  local httpCodeResponse=$(curl -sLo "${tempFile}" -w "%{http_code}" "${1}")
-  [ ${httpCodeResponse} -ne 200 ] && echo "Invalid URL: ${1}" && exit 1
-  [ -f $tempFile ] && install -D -m 755 $tempFile "${2}"
+  tempFile=$(mktemp -u)
+  httpCodeResponse=$(curl -sLo "${tempFile}" -w "%{http_code}" "${1}")
+  [ "${httpCodeResponse}" -ne 200 ] && printf "%s\n" "Invalid URL: $1" && exit 1
+  [ -f "$tempFile" ] && install -D -m 755 "$tempFile" "${2}"
 }
 
 importer() { # ${1} command-name; ${2} command-url
 
-  local commandName=${1:?"Command name not defined"}
-  local commandUrl=${2:?"Github url not defined"}
+  commandName=${1:?"Command name not defined"}
+  commandUrl=${2:?"Github url not defined"}
 
-  local withoutPrefix=${commandUrl/#https:\/\/github.com\//}
-  local projectDir=$(echo $withoutPrefix | cut -d '/' -f 1,2)
+  withoutPrefix=$(echo "$commandUrl" | sed -e 's/https\:\/\/github.com\///')
+  projectDir=$(echo "$withoutPrefix" | cut -d '/' -f 1,2)
 
-  local githubCommand=$(printf ${GITHUB_SRC} ${projectDir} ${commandName})
-  local githubUsage=$(printf ${GITHUB_USAGE} ${projectDir} ${commandName})
+  githubCommand=$(printf "https://github.com/%s/raw/main/src/%s" "${projectDir}" "${commandName}")
+  # githubUsage=$(printf "https://github.com/%s/raw/main/usage/${LANG}/%s" "${projectDir}" "${commandName}")
 
-  local projcetFile="${IMPORT_PATH}/${projectDir}/src/${commandName}"
-  local projcetUsage="${IMPORT_PATH}/${projectDir}/usage/${LANG}/${commandName}"
+  projcetFile="${IMPORT_PATH}/${projectDir}/src/${commandName}"
+  # projcetUsage="${IMPORT_PATH}/${projectDir}/usage/${LANG}/${commandName}"
   
   download "${githubCommand}" "${projcetFile}"
 
@@ -80,14 +77,15 @@ launch() {
 
   [ -z "$1" ] && usage && return 0;
 
-  local target=$1
+  target=$1
 
-  [ "$target" == "import" ] && importer "$2" "$3" && return 0
+  [ "$target" = "import" ] && importer "$2" "$3" && return 0
 
   for module in "${IMPORT_PATH}"/*/*
   do
     if [ -f "${module}/src/${target}" ] ; then
-      . "${module}/src/${target}" ${@:2}
+      # shellcheck source=/dev/null
+      . "${module}/src/${target}" "${@:2}"
       return 0
     fi
   done
@@ -104,10 +102,10 @@ while getopts ':vh-:' OPTION ; do
     case "$OPTION" in
     h ) usage  ;;
     v ) version ;;
-    - ) [ $OPTIND -ge 1 ] && optind=$(expr $OPTIND - 1 ) || optind=$OPTIND
+    - ) [ $OPTIND -ge 1 ] && optind=$((OPTIND - 1 )) || optind=$OPTIND
          eval OPTION="\$$optind"
-         OPTARG=$(echo $OPTION | cut -d'=' -f2)
-         OPTION=$(echo $OPTION | cut -d'=' -f1)
+         OPTARG=$(echo "$OPTION" | cut -d'=' -f2)
+         OPTION=$(echo "$OPTION" | cut -d'=' -f1)
          case $OPTION in
             --help) usage ;;
             --version) version ;;
@@ -120,4 +118,4 @@ while getopts ':vh-:' OPTION ; do
     esac
 done
 
-launch $@
+launch "$@"
